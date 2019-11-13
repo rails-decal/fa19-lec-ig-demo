6165c47c8c64786b9379b8c038777a3550dc43e1
kyuwonk <freddy.kim8@gmail.com>
2019-11-13 01:59:05 -0800

    Add devise, faker, and likes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |index|
+  email = Faker::Internet.email
+  password = Faker::Internet.password
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    bio: Faker::Quote.famous_last_words,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+  puts "Created user with #{email} and #{password}"
+end
+
+50.times do |index|
+  Post.create!(
+    user_id: Faker::Number.between(from: 1, to: User.count),
+    caption: Faker::Quote.famous_last_words
+  )
+end
+
+100.times do |index|
+  Like.create!(
+    user_id: Faker::Number.between(from: 1, to: User.count),
+    post_id: Faker::Number.between(from: 1, to: Post.count)
+  )
+end
